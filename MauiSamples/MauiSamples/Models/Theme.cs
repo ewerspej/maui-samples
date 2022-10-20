9ca53bb375db8497bc5f2144f091b0118cf26ad7
Julian Ewers <ewerspej@tcd.ie>
2022-10-20 12:35:17 +0200

    added display name

--- MauiSamples/MauiSamples/Models/Theme.cs
+++ MauiSamples/MauiSamples/Models/Theme.cs
@@ -2,16 +2,18 @@
 
 public sealed class Theme
 {
-    public static Theme Dark = new(0, nameof(Dark));
-    public static Theme Light = new(1, nameof(Light));
-    public static Theme System = new(2, nameof(System));
+    public static Theme Dark = new(0, nameof(Dark), "Night Mode");
+    public static Theme Light = new(1, nameof(Light), "Day Mode");
+    public static Theme System = new(2, nameof(System), "Follow System");
 
     public int Id { get; }
     public string Name { get; }
+    public string DisplayName { get; }
 
-    private Theme(int id, string name)
+    private Theme(int id, string name, string displayName)
     {
         Id = id;
         Name = name;
+        DisplayName = displayName;
     }
 }
