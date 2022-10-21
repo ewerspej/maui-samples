32935fd02b9d9d0f841c6d0dd832abde5d783b4a
Julian Ewers <ewerspej@tcd.ie>
2022-10-21 09:11:00 +0200

    removed unused nuget

--- MauiSamples/MauiSamples/MauiProgram.cs
+++ MauiSamples/MauiSamples/MauiProgram.cs
@@ -1,5 +1,4 @@
-﻿using CommunityToolkit.Maui;
-using Microsoft.Extensions.Logging;
+﻿using Microsoft.Extensions.Logging;
 
 namespace MauiSamples;
 
@@ -10,7 +9,6 @@ public static class MauiProgram
         var builder = MauiApp.CreateBuilder();
         builder
             .UseMauiApp<App>()
-            .UseMauiCommunityToolkit()
             .ConfigureFonts(fonts =>
             {
                 fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
