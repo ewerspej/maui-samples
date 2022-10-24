edccc0286230c594f9a6b3b1d4a00bdba683bf33
Julian Ewers <ewerspej@tcd.ie>
2022-10-24 09:51:05 +0200

    added code to hide system bars in MainActivity

--- MauiSamples/MauiSamples/Platforms/Android/MainActivity.cs
+++ MauiSamples/MauiSamples/Platforms/Android/MainActivity.cs
@@ -1,10 +1,22 @@
 ﻿using Android.App;
 using Android.Content.PM;
 using Android.OS;
+using Android.Views;
 
 namespace MauiSamples;
 
 [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
 public class MainActivity : MauiAppCompatActivity
 {
+    protected override void OnCreate(Bundle savedInstanceState)
+    {
+        base.OnCreate(savedInstanceState);
+        
+//#if ANDROID30_0_OR_GREATER
+//        if (Build.VERSION.SdkInt >= BuildVersionCodes.R) //R == API level 30.0
+//        {
+//            Window?.InsetsController?.Hide(WindowInsets.Type.SystemBars());
+//        }
+//#endif
+    }
 }
