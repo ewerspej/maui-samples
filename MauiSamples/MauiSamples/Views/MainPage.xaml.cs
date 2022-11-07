adac81b3553dde08b82773750eca95757ebe72af
Julian Ewers <ewerspej@tcd.ie>
2022-11-07 11:21:32 +0100

    added simple implementation of hello from platform

--- MauiSamples/MauiSamples/Views/MainPage.xaml.cs
+++ MauiSamples/MauiSamples/Views/MainPage.xaml.cs
@@ -6,5 +6,18 @@ public partial class MainPage : ContentPage
     {
         InitializeComponent();
     }
+
+    private async void Button_OnPressed(object sender, EventArgs e)
+    {
+#if ANDROID
+        await DisplayAlert("Hello", "Hello from Android!", "OK");
+#elif IOS
+        await DisplayAlert("Hello", "Hello from iOS!", "OK");
+#elif WINDOWS
+        await DisplayAlert("Hello", "Hello from Windows!", "OK");
+#else
+        await DisplayAlert("Hello", "Hello from another platform (MacCatalyst, Tizen, ...)", "OK");
+#endif
+    }
 }
 
