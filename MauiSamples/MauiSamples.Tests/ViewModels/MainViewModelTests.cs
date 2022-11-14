a45595e48b2c5af66f67e17b9383b132ceb85c62
Julian Ewers <ewerspej@tcd.ie>
2022-11-14 13:49:35 +0100

    added Moq and MainViewModelTests class

---
+++ MauiSamples/MauiSamples.Tests/ViewModels/MainViewModelTests.cs
@@ -0,0 +1,37 @@
+﻿using MauiSamples.Services.Device;
+using MauiSamples.ViewModels;
+using Moq;
+
+namespace MauiSamples.Tests.ViewModels;
+
+[TestFixture]
+public class MainViewModelTests
+{
+    [Test]
+    public void SetHighBrightness_SetScreenBrightnessCalled()
+    {
+        //arrange
+        var deviceServiceMock = new Mock<IDeviceService>();
+        var vm = new MainViewModel(deviceServiceMock.Object);
+
+        //act
+        vm.SetHighBrightness();
+
+        //assert
+        deviceServiceMock.Verify(service => service.SetScreenBrightness(It.IsAny<float>()), Times.Once);
+    }
+
+    [Test]
+    public void SetLowBrightness_SetScreenBrightnessCalled()
+    {
+        //arrange
+        var deviceServiceMock = new Mock<IDeviceService>();
+        var vm = new MainViewModel(deviceServiceMock.Object);
+
+        //act
+        vm.SetLowBrightness();
+
+        //assert
+        deviceServiceMock.Verify(service => service.SetScreenBrightness(It.IsAny<float>()), Times.Once);
+    }
+}
