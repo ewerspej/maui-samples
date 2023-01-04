433038f1eb1b18eeada79d3264c10ed119d3f198
Julian Ewers <ewerspej@tcd.ie>
2023-01-04 09:21:02 +0100

    made static Theme fields readonly

--- MauiSamples/MauiSamples/Models/Theme.cs
+++ MauiSamples/MauiSamples/Models/Theme.cs
@@ -2,9 +2,9 @@
 
 public sealed class Theme
 {
-    public static Theme Dark = new(AppTheme.Dark, "Night Mode");
-    public static Theme Light = new(AppTheme.Light, "Day Mode");
-    public static Theme System = new(AppTheme.Unspecified, "Follow System");
+    public static readonly Theme Dark = new(AppTheme.Dark, "Night Mode");
+    public static readonly Theme Light = new(AppTheme.Light, "Day Mode");
+    public static readonly Theme System = new(AppTheme.Unspecified, "Follow System");
 
     public static List<Theme> AvailableThemes { get; } = new()
     {
