2392284d1dcca7473131a239e24c2969848f44d6
Julian Ewers <ewerspej@tcd.ie>
2023-01-23 11:40:10 +0100

    implemented BindingsPage and ViewModel to demonstrate compiled bindings

---
+++ MauiSamples/MauiSamples/Models/BindingItem.cs
@@ -0,0 +1,7 @@
+﻿namespace MauiSamples.Models;
+
+public class BindingItem
+{
+    public string Name { get; set; }
+    public int Count { get; set; }
+}
--- MauiSamples/MauiSamples/ViewModels/BindingsViewModel.cs
+++ MauiSamples/MauiSamples/ViewModels/BindingsViewModel.cs
@@ -1,8 +1,33 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using MauiSamples.Models;
+using System.Collections.ObjectModel;
 
 namespace MauiSamples.ViewModels;
 
 public partial class BindingsViewModel : ObservableObject
 {
-    
+    [ObservableProperty]
+    private ObservableCollection<BindingItem> _items = new();
+
+    [RelayCommand]
+    private void AddItem()
+    {
+        Items.Add(new BindingItem
+        {
+            Name = $"Item {Items.Count}",
+            Count = Items.Count
+        });
+    }
+
+    [RelayCommand]
+    private void RemoveItem()
+    {
+        if(Items.Count == 0)
+        {
+            return;
+        }
+
+        Items.Remove(Items.Last());
+    }
 }
