c40a56303882a6443d466bd4386ef51375a05d22
Julian Ewers <ewerspej@tcd.ie>
2023-10-16 12:09:36 +0200

    added platform-specific view example using <OnPlatform>

---
+++ MauiSamples/MauiSamples/Views/Platform/Android/ImageViewAndroid.xaml.cs
@@ -0,0 +1,9 @@
+namespace MauiSamples.Views.Platform.Android;
+
+public partial class ImageViewAndroid : ContentView
+{
+	public ImageViewAndroid()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MauiSamples/MauiSamples/Views/Platform/iOS/ImageViewiOS.xaml.cs
@@ -0,0 +1,9 @@
+namespace MauiSamples.Views.Platform.iOS;
+
+public partial class ImageViewiOS : ContentView
+{
+	public ImageViewiOS()
+	{
+		InitializeComponent();
+	}
+}
