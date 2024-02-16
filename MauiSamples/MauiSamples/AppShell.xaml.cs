e6b274bb51a7ed353b5840f047e57755a8617802
Julian Ewers-Peters <ewerspej@tcd.ie>
2024-02-16 12:37:41 +0100

    replaced manual route registration with automatic registration using the latest preview version of epj.RouteGenerator (1.0.1-alpha2)

--- MauiSamples/MauiSamples/AppShell.xaml.cs
+++ MauiSamples/MauiSamples/AppShell.xaml.cs
@@ -14,9 +14,14 @@ public partial class AppShell : Shell
         //Routing.RegisterRoute(nameof(AccordionOnePage), typeof(AccordionOnePage));
         //Routing.RegisterRoute(nameof(AccordionMvvmPage), typeof(AccordionMvvmPage));
 
-        Routing.RegisterRoute(Routes.MultiBindingPage, typeof(MultiBindingPage));
-        Routing.RegisterRoute(Routes.PassObjectsPage, typeof(PassObjectsPage));
-        Routing.RegisterRoute(Routes.AccordionOnePage, typeof(AccordionOnePage));
-        Routing.RegisterRoute(Routes.AccordionMvvmPage, typeof(AccordionMvvmPage));
+        //Routing.RegisterRoute(Routes.MultiBindingPage, typeof(MultiBindingPage));
+        //Routing.RegisterRoute(Routes.PassObjectsPage, typeof(PassObjectsPage));
+        //Routing.RegisterRoute(Routes.AccordionOnePage, typeof(AccordionOnePage));
+        //Routing.RegisterRoute(Routes.AccordionMvvmPage, typeof(AccordionMvvmPage));
+
+        foreach (var route in Routes.RouteTypeMap)
+        {
+            Routing.RegisterRoute(route.Key, route.Value);
+        }
     }
 }
