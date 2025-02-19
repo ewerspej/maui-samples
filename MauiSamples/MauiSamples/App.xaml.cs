4fbe047b7122908bf710834b228e9c31758ea606
Julian Ewers-Peters <ewerspej@tcd.ie>
2025-02-19 10:49:23 +0100

    cleanup

--- MauiSamples/MauiSamples/App.xaml.cs
+++ MauiSamples/MauiSamples/App.xaml.cs
@@ -9,15 +9,15 @@ public partial class App : Application
     public App()
     {
         InitializeComponent();
-
-        MainPage = new AppShell();
-
+        
         // let's set the initial theme already during the app start
         SetTheme();
 
         // subscribe to changes in the settings
-        SettingsService.Instance.PropertyChanged += OnSettingsPropertyChanged;
+        SettingsService.Instance.PropertyChanged += OnSettingsPropertyChanged!;
     }
+
+    protected override Window CreateWindow(IActivationState? activationState) => new(new AppShell());
 
     private void OnSettingsPropertyChanged(object sender, PropertyChangedEventArgs e)
     {
