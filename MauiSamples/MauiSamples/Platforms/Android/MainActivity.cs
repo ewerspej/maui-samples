4c70e122684ad1c4432cb54da4ff7d7ae5c906a9
Julian Ewers-Peters <ewerspej@tcd.ie>
2025-02-21 09:37:47 +0100

    fixed MediaElement on Android

--- MauiSamples/MauiSamples/Platforms/Android/MainActivity.cs
+++ MauiSamples/MauiSamples/Platforms/Android/MainActivity.cs
@@ -3,7 +3,7 @@ using Android.Content.PM;
 
 namespace MauiSamples;
 
-[Activity(Theme = "@style/MyTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+[Activity(Theme = "@style/MyTheme", ResizeableActivity = true, MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
 public class MainActivity : MauiAppCompatActivity
 {
 }
